ececb0578ba5eeda64c8b042dbd9d72315550dde
Mark Ziegler <mark.ziegler@koerber.com>
2014-09-09 06:27:10 +0000

    update from pvcs (only version number)

--- SVObserver/AutoBuild.h
+++ SVObserver/AutoBuild.h
@@ -5,8 +5,8 @@
 //* .Module Name     : AutoBuild
 //* .File Name       : $Workfile:   AutoBuild.h  $
 //* ----------------------------------------------------------------------------
-//* .Current Version : $Revision:   1.50  $
-//* .Check In Date   : $Date:   03 Sep 2014 16:17:46  $
+//* .Current Version : $Revision:   1.51  $
+//* .Check In Date   : $Date:   08 Sep 2014 10:45:58  $
 //******************************************************************************
 
 #pragma once
@@ -23,7 +23,7 @@
 //    c is the beta version number - if c equals 0 or is greater than 254 then the application is marked as Released
 //    d is unused
 
-#define FILEVER        6,20,27,0
+#define FILEVER        6,20,28,0
 #define PRODUCTVER     FILEVER
 #define STRFILEVER     SV_STRINGIZE( FILEVER )
 #define STRPRODUCTVER  SV_STRINGIZE( PRODUCTVER )
@@ -33,6 +33,16 @@
 //******************************************************************************
 /*
 $Log:   N:\PVCSarch65\ProjectFiles\archives\SVObserver_SRC\SVObserver\AutoBuild.h_v  $
+ * 
+ *    Rev 1.51   08 Sep 2014 10:45:58   tbair
+ * Project:  SVObserver
+ * Change Request (SCR) nbr:  865
+ * SCR Title:  Update SVObserver Version Number for Fletch Release
+ * Checked in by:  tBair;  Tom Bair
+ * Change Description:  
+ *   Changed version to 6.20 Beta 028
+ * 
+ * /////////////////////////////////////////////////////////////////////////////////////
  * 
  *    Rev 1.50   03 Sep 2014 16:17:46   bwalter
  * Project:  SVObserver
