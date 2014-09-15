9e32d6628232fed091893251f82971744f6cd732
Mark Ziegler <mark.ziegler@koerber.com>
2014-09-15 08:39:17 +0000

    last update for 6.20 from pvcs

--- RunRejectServer/AutoBuild.h
+++ RunRejectServer/AutoBuild.h
@@ -5,8 +5,8 @@
 //* .Module Name     : AutoBuild
 //* .File Name       : $Workfile:   AutoBuild.h  $
 //* ----------------------------------------------------------------------------
-//* .Current Version : $Revision:   1.2  $
-//* .Check In Date   : $Date:   04 Sep 2014 14:33:24  $
+//* .Current Version : $Revision:   1.3  $
+//* .Check In Date   : $Date:   11 Sep 2014 15:08:50  $
 //******************************************************************************
 
 #pragma once
@@ -23,7 +23,7 @@
 //    c is the beta version number - if c equals 0 or is greater than 254 then the application is marked as Released
 //    d is unused
 
-#define FILEVER        1,0,3,0
+#define FILEVER        6,20,255,0
 #define PRODUCTVER     FILEVER
 #define STRFILEVER     SV_STRINGIZE( FILEVER )
 #define STRPRODUCTVER  SV_STRINGIZE( PRODUCTVER )
@@ -33,6 +33,16 @@
 //******************************************************************************
 /*
 $Log:   N:\PVCSarch65\ProjectFiles\archives\SVObserver_SRC\RunRejectServer\AutoBuild.h_v  $
+ * 
+ *    Rev 1.3   11 Sep 2014 15:08:50   bwalter
+ * Project:  SVObserver
+ * Change Request (SCR) nbr:  865
+ * SCR Title:  Update SVObserver Version Number for Fletch Release
+ * Checked in by:  tBair;  Tom Bair
+ * Change Description:  
+ *   Changed version to 6.20 Released (beta 255)
+ * 
+ * /////////////////////////////////////////////////////////////////////////////////////
  * 
  *    Rev 1.2   04 Sep 2014 14:33:24   bwalter
  * Project:  SVObserver
