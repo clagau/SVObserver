b3e0cd7eef55a3fa7520189340cdc17927b78a9b
Mark Ziegler <mark.ziegler@koerber.com>
2014-09-16 06:11:37 +0000

    update from PVCS (update version numbers to 6.30)

--- SVObserver/AutoBuild.h
+++ SVObserver/AutoBuild.h
@@ -5,8 +5,8 @@
 //* .Module Name     : AutoBuild
 //* .File Name       : $Workfile:   AutoBuild.h  $
 //* ----------------------------------------------------------------------------
-//* .Current Version : $Revision:   1.52  $
-//* .Check In Date   : $Date:   09 Sep 2014 09:31:54  $
+//* .Current Version : $Revision:   1.53  $
+//* .Check In Date   : $Date:   15 Sep 2014 16:39:10  $
 //******************************************************************************
 
 #pragma once
@@ -23,7 +23,7 @@
 //    c is the beta version number - if c equals 0 or is greater than 254 then the application is marked as Released
 //    d is unused
 
-#define FILEVER        6,20,255,0
+#define FILEVER        6,30,1,0
 #define PRODUCTVER     FILEVER
 #define STRFILEVER     SV_STRINGIZE( FILEVER )
 #define STRPRODUCTVER  SV_STRINGIZE( PRODUCTVER )
@@ -33,6 +33,16 @@
 //******************************************************************************
 /*
 $Log:   N:\PVCSarch65\ProjectFiles\archives\SVObserver_SRC\SVObserver\AutoBuild.h_v  $
+ * 
+ *    Rev 1.53   15 Sep 2014 16:39:10   bwalter
+ * Project:  SVObserver
+ * Change Request (SCR) nbr:  940
+ * SCR Title:  Update SVObserver Version Number for the 6.30 Release
+ * Checked in by:  bWalter;  Ben Walter
+ * Change Description:  
+ *   Changed version to 6.30 Beta 001
+ * 
+ * /////////////////////////////////////////////////////////////////////////////////////
  * 
  *    Rev 1.52   09 Sep 2014 09:31:54   tbair
  * Project:  SVObserver
