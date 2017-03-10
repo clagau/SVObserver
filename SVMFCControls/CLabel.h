b855c5215958dbff973ee1096bd9161b1ba1c524
Arvid Breitenbach <Arvid.Breitenbach@koerber.com>
2017-03-10 09:01:33 +0000

    Change Observer for Visual Studio 2015 Issue SVO-1292

--- SVMFCControls/CLabel.h
+++ SVMFCControls/CLabel.h
@@ -23,10 +23,10 @@ class CLabel : public CStatic
 {
 // Construction
 public:
-	static enum LinkStyle { LinkNone, HyperLink, MailLink };
-	static enum FlashType { None, Text, Background };
-	static enum Type3D { Raised, Sunken };
-	static enum BackFillMode { Normal, Gradient };
+	enum LinkStyle { LinkNone, HyperLink, MailLink };
+	enum FlashType { None, Text, Background };
+	enum Type3D { Raised, Sunken };
+	enum BackFillMode { Normal, Gradient };
 
 	CLabel();
 	virtual ~CLabel();
