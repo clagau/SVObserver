0851fca201e3275084c4ae2b37b01a2bf3cee00e
Arvid Breitenbach <Arvid.Breitenbach@koerber.com>
2018-11-21 15:34:28 +0000

    Remove NTBackup functionality from SVObserver Utilities Issue SVO-1639

--- bin/svfullbackup.bat
+++
@@ -1,3 +0,0 @@
-eventcreate /ID 13 /L APPLICATION /T INFORMATION  /SO SVObserver.exe /D "SVFullBackup started"
-wbadmin start backup -backupTarget:f: -allCritical -quiet 
-eventcreate /ID 13 /L APPLICATION /T INFORMATION  /SO SVObserver.exe /D "SVFullBackup completed"
