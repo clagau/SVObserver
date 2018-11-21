eec6805fc0845efbb47629b6515b2c0cfb6a8e2d
VisionBuild <VisionBuild>
2018-11-21 14:52:19 +0000

    SVO_8.20_Beta003 Updating SVRemoteControl Autobuild.h

--- SVRemoteControl/AutoBuild.h
+++ SVRemoteControl/AutoBuild.h
@@ -24,7 +24,7 @@
 //    d is unused
 
 
-#define FILEVER        8,20,002,0
+#define FILEVER        8,20,003,0
 #define PRODUCTVER     FILEVER
 #define STRFILEVER     SV_STRINGIZE( FILEVER )
 #define STRPRODUCTVER  SV_STRINGIZE( PRODUCTVER )
