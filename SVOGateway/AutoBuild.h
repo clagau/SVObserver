282dffc5fea025f1b4d5bde40e3d277a149d283d
VisionBuild <VisionBuild>
2021-12-10 15:52:50 +0000

    SVO_10.20.0_Beta018 Updating Autobuild.h

--- SVOGateway/AutoBuild.h
+++ SVOGateway/AutoBuild.h
@@ -26,7 +26,7 @@
 //    the application is marked as Released
 //    d is unused
 
-#define FILEVER        10,20,0,17
+#define FILEVER        10,20,0,18
 #define PRODUCTVER FILEVER
 #define STRFILEVER SV_STRINGIZE(FILEVER)
 #define STRPRODUCTVER SV_STRINGIZE(PRODUCTVER)
