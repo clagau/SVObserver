39a3ed1bf017bca66294b99bf6eb64d7c8a179da
VisionBuild <VisionBuild>
2022-02-18 03:52:54 +0000

    SVO_10.20.0_Beta030 Updating Autobuild.h

--- SVRemoteControl/AutoBuild.h
+++ SVRemoteControl/AutoBuild.h
@@ -24,7 +24,7 @@
 //    d is used for the revision number underlying the build (if zero it is ignored)
 
 
-#define FILEVER        10,20,0,29
+#define FILEVER        10,20,0,30
 #define PRODUCTVER     FILEVER
 #define STRFILEVER     SV_STRINGIZE( FILEVER )
 #define STRPRODUCTVER  SV_STRINGIZE( PRODUCTVER )
